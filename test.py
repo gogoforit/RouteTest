b714afc14e403f8ba6f4b694e77513c40df08d58
wsx <715157026@qq.com>
2017-03-23 12:25:53 +0800

    添加excel导入程序

--- test.py
+++ test.py
@@ -5,86 +5,88 @@ import re
 from MongodbConn import MongoPipeline
 import time
 import my_cmdcode
-s = requests.session()
-#登陆路由器后台要post出去的数据
-data = {
-    'login':{'password':"WlD8wX02ceefbwK"}
-,
-'method':"do"
-}
-#头标签
-header = {
-"Host":"192.168.1.1",
-"User-Agent":"Mozilla/5.0 (Windows NT 10.0; WOW64; rv,:52.0) Gecko/20100101 Firefox/52.0",
-"Accept":"application/json, text/javascript, */*; q=0.01",
-"Accept-Language":"zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3",
-"Accept-Encoding":"gzip, deflate",
-"Content-Type":"application/json; charset=UTF-8",
-"X-Requested-With":"XMLHttpRequest",
-"Referer":"http://192.168.1.1/",
-"Content-Length":"54",
-"Connection":"keep-alive",
-}
-#post数据，登陆路由器后台控制界面
-html = s.post('http://192.168.1.1/',data=json.dumps(data),headers = header).json()
+while True:
+    s = requests.session()
+    #登陆路由器后台要post出去的数据
+    data = {
+        'login':{'password':"WlD8wX02ceefbwK"}
+    ,
+    'method':"do"
+    }
+    #头标签
+    header = {
+    "Host":"192.168.1.1",
+    "User-Agent":"Mozilla/5.0 (Windows NT 10.0; WOW64; rv,:52.0) Gecko/20100101 Firefox/52.0",
+    "Accept":"application/json, text/javascript, */*; q=0.01",
+    "Accept-Language":"zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3",
+    "Accept-Encoding":"gzip, deflate",
+    "Content-Type":"application/json; charset=UTF-8",
+    "X-Requested-With":"XMLHttpRequest",
+    "Referer":"http://192.168.1.1/",
+    "Content-Length":"54",
+    "Connection":"keep-alive",
+    }
+    #post数据，登陆路由器后台控制界面
+    html = s.post('http://192.168.1.1/',data=json.dumps(data),headers = header).json()
 
-#post获取在线的设备MAC地址等信息的包
-datahost = {
-'hosts_info':{'table':"online_host"},
-'method':"get"
-}
-stok = html['stok']
-nexturl = 'http://192.168.1.1/stok=' + stok + '/ds'
-# nexturl = 'http://192.168.1.1/stok=7%5BuVF%7CF%24v6W3HvDXpeiKr%2Bc%2BsoSrHPA%3E/ds'
-#获取在线设备等的MAC地址
-hostinfo = s.post(nexturl,data=json.dumps(datahost)).json()
-info = json.dumps(hostinfo)
-#正则匹配出所有的MAC地址
-macs = re.findall('"mac": "(.*?)"',info,re.S)
+    #post获取在线的设备MAC地址等信息的包
+    datahost = {
+    'hosts_info':{'table':"online_host"},
+    'method':"get"
+    }
+    stok = html['stok']
+    nexturl = 'http://192.168.1.1/stok=' + stok + '/ds'
+    # nexturl = 'http://192.168.1.1/stok=7%5BuVF%7CF%24v6W3HvDXpeiKr%2Bc%2BsoSrHPA%3E/ds'
+    #获取在线设备等的MAC地址
+    hostinfo = s.post(nexturl,data=json.dumps(datahost)).json()
+    info = json.dumps(hostinfo)
+    #正则匹配出所有的MAC地址
+    macs = re.findall('"mac": "(.*?)"',info,re.S)
 
-#存入数据库
-conn = MongoPipeline()
-conn.open_connection('qiandao')
-for each in macs: #把所有现在在线MAC地址都存入数据库中
-    print(each)
-    dic = {}
-    dic['mac'] = each
-    dic['_id'] = each
-    dic['_type'] = 'mac'
-    ids = conn.getIds('info', {'_id': each})
-    id = next(ids,None)
-    mytime  = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
-    print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
-    dic['time'] = mytime
-    if id!=None:
-        conn.update_item({'_id': each}, {"$set": {"num": id['num']+1}}, 'info')
-    else:
-        dic['num'] = 1
-        conn.process_item(dic, 'info')
+    #存入数据库
+    conn = MongoPipeline()
+    conn.open_connection('qiandao')
+    for each in macs: #把所有现在在线MAC地址都存入数据库中
+        print(each)
+        dic = {}
+        dic['mac'] = each
+        dic['_id'] = each
+        dic['_type'] = 'mac'
+        ids = conn.getIds('info', {'_id': each})
+        id = next(ids,None)
+        mytime  = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+        print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
+        dic['time'] = mytime
+        if id!=None:
+            conn.update_item({'_id': each}, {"$set": {"num": id['num']+1}}, 'info')
+        else:
+            dic['num'] = 1
+            conn.process_item(dic, 'info')
 
-#统计哪些人到了，用mac地址和已经存好的姓名对应起来
-conn = MongoPipeline()
-conn.open_connection('qiandao')
-ids = conn.getIds('info', {'_type': 'mac'})
-_id = next(ids, None)
-while _id:
-    print(_id)
-    dic = {}
-    mac = _id['mac']
-    dic['mac'] = _id['mac']
-    dic['time'] = _id['time']
-    conn2 = MongoPipeline()
-    conn2.open_connection('qiandao_mac_name') #conn2储存的mac地址和对应的名字
-    searchInfo = conn2.getIds('info',{'mac': mac})
-    theInfo = next(searchInfo,None)
-    print(theInfo)
-    conn3 = MongoPipeline()#conn3对应最后的结果，结果导出到csv文件
-    conn3.open_connection('qiandao_last_info')
-    if theInfo!=None:
-        dic['name'] = theInfo['name']
-        dic['_id'] = theInfo['name']
-        conn3.process_item(dic, 'info')
+    #统计哪些人到了，用mac地址和已经存好的姓名对应起来
+    conn = MongoPipeline()
+    conn.open_connection('qiandao')
+    ids = conn.getIds('info', {'_type': 'mac'})
     _id = next(ids, None)
+    while _id:
+        print(_id)
+        dic = {}
+        mac = _id['mac']
+        dic['mac'] = _id['mac']
+        dic['time'] = _id['time']
+        conn2 = MongoPipeline()
+        conn2.open_connection('qiandao_mac_name') #conn2储存的mac地址和对应的名字
+        searchInfo = conn2.getIds('info',{'mac': mac})
+        theInfo = next(searchInfo,None)
+        print(theInfo)
+        print(123)
+        conn3 = MongoPipeline()#conn3对应最后的结果，结果导出到csv文件
+        conn3.open_connection('qiandao_last_info')
+        if theInfo!=None:
+            dic['name'] = theInfo['name']
+            dic['_id'] = theInfo['name']
+            conn3.process_item(dic, 'info')
+        _id = next(ids, None)
 
 my_cmdcode.output() #导出csv格式文件
 #
