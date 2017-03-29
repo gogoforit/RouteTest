d6453db27d6ab44a077a3d12496d32f8608cb0fc
gogoforit <715157026@qq.com>
2017-03-29 21:41:59 +0800

    对课程做分类处理

--- my_cmdcode.py
+++ my_cmdcode.py
@@ -4,4 +4,4 @@ import os
 #命令行导出csv文件
 def output():
    # os.chdir( r"H:\mong\bin")
-    os.system('mongoexport -d qiandao_last_info -c info --csv -f time,mac,name -o qiandao_last_info.csv')
+    os.system('mongoexport -d qiandao_last_info -c info --csv -f time,mac,class_num,name -o qiandao_last_info.csv')
