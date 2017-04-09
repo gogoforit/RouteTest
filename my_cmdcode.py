93f2dbbf2ae04ca3b83e1eebfbe5f29d95b5b8e7
gogoforit <715157026@qq.com>
2017-04-09 15:08:31 +0800

    修改了打印文件的路径问题

--- my_cmdcode.py
+++ my_cmdcode.py
@@ -3,5 +3,7 @@ import os
 # os.system(commands)
 #命令行导出csv文件
 def output():
-    os.chdir( r"/home/kk/python_code/theinfo/RouteTest")
+    # os.chdir( r"/home/kk/python_code/theinfo/RouteTest")
+    cwd = os.getcwd()
+    os.chdir(cwd)
     os.system('mongoexport -d qiandao_last_info -c info --csv -f time,mac,class_num,name,studentid -o qiandao_last_info.csv')
